5c696bb6b7567bb534ce47e85598799edb05aaa2
Shishira <shishiraiyar@gmail.com>
2024-02-04 02:33:58 +0530

    Raspberry pi code

---
+++ pi/genAI.py
@@ -0,0 +1,75 @@
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel, Part
+from google.oauth2 import service_account
+from geopy.geocoders import Nominatim
+
+
+credentials = service_account.Credentials.from_service_account_file('./creds.json')
+vertexai.init( project="enigma-jit", credentials=credentials)
+model = GenerativeModel("gemini-pro")
+chat = model.start_chat()
+
+config = {
+    "max_output_tokens": 2048,
+    "temperature": 0.9,
+    "top_p": 1
+}
+geolocator = Nominatim(user_agent="accident-detector")
+	
+
+
+def getGeminiResponse(prompt : str) -> str:
+    response = chat.send_message(prompt, generation_config=config).candidates[0].content.parts[0].text
+    return response
+
+def getAddressFromCoordinates(lat:int, lng:int) -> str:
+    location = geolocator.reverse(f"{float(lat)}, {float(lng)}")
+    return str(location)
+    # return "DTL RVCE Bangalore"
+
+def getPromptFromData(data):
+    prompt = "Give a distress message for the following data. Only give the message and nothing else. "
+    prompt += "There are people stranded at "
+    prompt += getAddressFromCoordinates(data["lat"], data["lon"])
+    prompt += "(simplify the address). "
+    prompt += "There are " + data['quantity'] + " people among them there are "
+
+    prompt += "women, " if ("women" in data) else ""
+    prompt += "children, " if ("children" in data) else ""
+    prompt += "disabled people, " if ("disabled" in data) else ""
+    prompt += "injured people, " if ("injured" in data) else ""
+    
+    prompt += ". They need "
+    prompt += "medicines, " if ("medicine" in data) else ""
+    prompt += "food, " if ("food" in data) else ""
+    prompt += "other help, " if ("other" in data) else ""   # TRY REMOVING
+    
+    prompt += ". They also mentioned "
+    prompt += data['chall']
+    
+    return prompt
+
+
+sampleData = {'medical': 'on', 
+ 'food': 'on', 
+ 'other': 'on', 
+
+ 'quantity': '100', 
+
+ 'women': 'on', 
+ 'children': 'on', 
+ 'disabled': 'on', 
+ 'injured': 'on', 
+
+ 'chall': 'Disaster Ultra Pro Max', 
+ 'lat': '12.923311', 
+ 'lon': '77.497898'}
+
+
+if __name__ == "__main__":
+    prompt = getPromptFromData(sampleData)
+    # print(getAddressFromCoordinates(sampleData["lat"], sampleData["lon"]))
+    print(getGeminiResponse(prompt))
+
+
+
---
+++ pi/main.py
@@ -0,0 +1,41 @@
+import firebase_admin
+from firebase_admin import db
+import serial
+import re
+import json
+import time
+
+from genAI import getPromptFromData, getGeminiResponse
+
+cred_obj = firebase_admin.credentials.Certificate('./lifebuoy-8161b-firebase-adminsdk-druf5-47f4d6d797.json')
+default_app = firebase_admin.initialize_app(cred_obj, {
+        'databaseURL':"https://lifebuoy-8161b-default-rtdb.firebaseio.com/"
+        })
+ref = db.reference("/test")
+
+
+ser = serial.Serial("/dev/ttyUSB0", 9600)
+pattern = re.compile(r"MESSAGE(.*?)ENDMESSAGE")
+
+while(1):
+    if (ser.in_waiting):
+        data = ser.readline().decode('utf-8')
+        if "MESSAGE" in data:
+            match = pattern.search(data)
+            result = match.group(1)
+            result = json.loads(result)
+
+            curTime = int(time.time())
+            
+            print(result)
+            prompt = getPromptFromData(result)
+            distressMsg = getGeminiResponse(prompt)
+            print(distressMsg)
+            # result["lat"] = "12.923311"
+            # result["lon"] = "77.497898"
+            childRef = ref.child(str(curTime))
+            childRef.set(result)
+        # print(data)
+
+    
+
