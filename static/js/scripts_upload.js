3a2aa5939eab73920c1244f8a5559ce7396ab579
Mohammed Ali Alsakkaf (Binbasri) <mohammed.binbasri@gmail.com>
2024-02-04 04:30:22 +0530

    Create scripts_upload.js

---
+++ static/js/scripts_upload.js
@@ -0,0 +1,5 @@
+function selectSignatureType(type) {
+    // Here you can define what happens when a signature type is selected
+    alert('Selected signature type: ' + type);
+    // You might want to redirect the user or update the UI accordingly
+  }
