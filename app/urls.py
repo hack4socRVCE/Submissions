6565817640b7eaba2ce21d10a778ac7389f35050
Mohammed Ali Alsakkaf (Binbasri) <mohammed.binbasri@gmail.com>
2024-02-04 04:28:17 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,17 @@ from . import views
 app_name = 'app'
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',views.home,name='home'),
+    path('home',views.home,name='home'),
     path('generate-pdf', views.generate_pdf, name='generate_pdf'),
     path('upload-pdf', views.upload_pdf, name='upload_pdf'),
     path('paraphrase', views.paraphrase_route, name='paraphrase'),
     path('summarise', views.summarise_route, name='summarise'),
     path('esign',views.esign,name='esign'),
+    path('index',views.index,name='index'),
+    path('signtype',views.signtype,name='signtype'),
+    path('login',views.login,name='login'),
+    path('register',views.register,name='register'),
+    path('forgot_password',views.forgot_password,name='forgot_password'),
+    path('upload_pdf_main',views.upload_pdf_main,name='upload_pdf_main'),
 ]
